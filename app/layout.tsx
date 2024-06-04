17ece12e5f40874fe51e08f35ff55d36037e202e
unknown <bipronilghosh@gmail.com>
2024-06-04 22:44:20 +0530

    first

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,10 +11,10 @@ const poppins = Poppins({
 })
 
 export const metadata: Metadata = {
-  title: 'Evently',
-  description: 'Evently is a platform for event management.',
+  title: 'EventPlanner360',
+  description: 'EventPlanner360 is a platform for event management.',
   icons: {
-    icon: '/assets/images/logo.svg'
+    icon: '/assets/images/logo.jpeg'
   }
 }
 
